c351e0c3c11f89319e96ba52eb4e91708bf76d19
empathicqubit <empathicqubit@entan.gl>
2023-01-06 22:01:43 +0100

    Add options to set archive and save appvars

--- bashpac8x.sh
+++ bashpac8x.sh
@@ -5,6 +5,10 @@ function make_little {
     printf "%04x" "$1" | fold -w2 | tac | tr -d "\n"
 }
 
+function make_hex {
+    printf "%02x" "$1"
+}
+
 function bytesum {
     {
         echo 'echo $(('
@@ -14,9 +18,31 @@ function bytesum {
 }
 
 if [[ "$1" == "--help" ]] ; then
-    >&2 echo "Syntax: $0 <program file path> [CALCNAME]"
-    exit 0;
+    >&2 echo "Syntax: $0 <program file path> [--variable] [--archive] [CALCNAME]"
+    >&2 echo "    --variable: Save as an appvar instead of a program"
+    >&2 echo "    --archive: Save as an *archive file"
+    exit 0
 fi
+
+TYPE=6
+VERSION=0
+ARCHIVE=0
+
+while [[ "$1" =~ -- ]] ; do
+    if [[ "$1" == "--variable" ]] ; then
+        >&2 echo "VARIABLING"
+        TYPE=21
+    fi
+    if [[ "$1" == "--archive" ]] ; then
+        >&2 echo "ARCHIVING"
+        ARCHIVE=128
+    fi
+    shift
+done
+
+TYPE_BYTE=$(make_hex $TYPE)
+VERSION_BYTE=$(make_hex $VERSION)
+ARCHIVE_BYTE=$(make_hex $ARCHIVE)
 
 INPUT_FILE="$1"
 INPUT_FILENAME="$(basename "${INPUT_FILE%.*}")"
@@ -31,36 +57,50 @@ COMMENT_HEX=$(echo -ne "$COMMENT" | xxd -ps)
 BINSIZE=$(cat "$INPUT_FILE" | wc --bytes)
 BINSIZE_LITTLE=$(make_little $BINSIZE)
 
-BINSIZE2=$((BINSIZE+2))
-BINSIZE2_LITTLE=$(make_little $BINSIZE2)
+if ((TYPE == 23)) ; then
+    VARDATA="$(cat "$INPUT_FILE" | xxd -ps)"
+    VARSIZE=$((BINSIZE))
+else
+    VARDATA="$BINSIZE_LITTLE $(cat "$INPUT_FILE" | xxd -ps)"
+    VARSIZE=$((BINSIZE+2))
+fi
 
-BINSIZE19=$((BINSIZE2+17))
-BINSIZE19_LITTLE=$(make_little $BINSIZE19)
+VARSIZE_LITTLE=$(make_little $VARSIZE)
+
+VARRECORD_SIZE=$((VARSIZE+17))
+VARRECORD_SIZE_LITTLE=$(make_little $VARRECORD_SIZE)
 
 FILE_SUM=$({ echo -ne "$BINSIZE_LITTLE" | xxd -r -ps ; cat "$INPUT_FILE" ; } | bytesum)
 NAME_SUM=$(echo -ne "$NAME" | bytesum)
 
-BINSIZE2_LO=$((BINSIZE2 & 0xff))
-BINSIZE2_HI=$((BINSIZE2 >> 8))
+VARHEADER_SIZE=13
+VARHEADER_SIZE_LITTLE=$(make_little $VARHEADER_SIZE)
+VARHEADER_SIZE_LO=$((VARHEADER_SIZE & 0xff))
+VARHEADER_SIZE_HI=$((VARHEADER_SIZE >> 8))
 
-HEADER_SUM=$((2*BINSIZE2_LO + 2*BINSIZE2_HI + 6 + 0 + 0 + 13 + 0))
+VARSIZE_LO=$((VARSIZE & 0xff))
+VARSIZE_HI=$((VARSIZE >> 8))
+
+HEADER_SUM=$((2*VARSIZE_LO + 2*VARSIZE_HI + TYPE + VERSION + ARCHIVE + VARHEADER_SIZE_LO + VARHEADER_SIZE_HI))
 
 SUM=$(((HEADER_SUM + NAME_SUM + FILE_SUM) % 65536))
 SUM_LITTLE=$(make_little $SUM)
 
+FILE_HEADER="$(echo -ne "**TI83F*" | xxd -ps)"
+
 cat <<HERE | xxd -r -ps
-2A 2A 54 49 38 33 46 2A
+$FILE_HEADER
 1A 0A 00
 $COMMENT_HEX
-$BINSIZE19_LITTLE
-0D 00
-$BINSIZE2_LITTLE
-06
+$VARRECORD_SIZE_LITTLE
+
+$VARHEADER_SIZE_LITTLE
+$VARSIZE_LITTLE
+$TYPE_BYTE
 $NAME_HEX
-00 00
-$BINSIZE2_LITTLE
-$BINSIZE_LITTLE
-$(cat "$INPUT_FILE" | xxd -ps)
+$VERSION_BYTE $ARCHIVE_BYTE
+$VARSIZE_LITTLE
+$VARDATA
 $SUM_LITTLE
 HERE
 
